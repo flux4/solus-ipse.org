171744895dc46fb74862e56abfa76dca40c99898
simian201 <simian201@gmail.com>
2017-06-14 02:42:16 -0700

    kybos v0.6

--- src/app/kybos/kybos.component.ts
+++ src/app/kybos/kybos.component.ts
@@ -2,7 +2,42 @@ import { Component, OnInit, NgZone } from '@angular/core';
 
 
 class Hypercube {
-  
+  public points: number[][];
+  public edges: number[][];
+  constructor(n_dimensions: number) {
+    var n_pts = 1 << n_dimensions;
+    this.points = [];
+    for (var i=0; i<n_pts; ++i) {
+      var t = i;
+      var p = [];
+      for (var j=0; j<n_pts; ++j) {
+        var v = (t%2 == 0)? -1: 1;
+        p.push(v);
+        t = Math.floor(t/2);
+      }
+      this.points.push(p);
+    }
+
+    // creat edges between vertices that only differ in a single dimension
+    this.edges = []; // n*(n-1)/2
+    for (var i=0; i<n_pts; ++i) {
+      for (var j=i+1; j<n_pts; ++j) {
+        var n_different = 0;
+        for (var k=0; k<n_dimensions; ++k) {
+          if (this.points[i][k] != this.points[j][k]) {
+            n_different++;
+            if (n_different >= 2) {
+              break;
+            }
+          }
+        }
+        if (n_different === 1) {
+          this.edges.push([i, j]);
+        }
+      }
+    }
+
+  }
 }
 
 
@@ -25,7 +60,7 @@ class Transform {
     this.update();
 
     this.animate = 0;
-    this.goal_angle = 0;
+    this.goal_angle = -1;
     this.angle_index = 0;
   }
   update() {
@@ -60,32 +95,36 @@ export class KybosComponent implements OnInit {
   };
 
   private transforms: Transform[];
-  private obj_pts: number[][];
-  private screen_pts: number[][];
+  private hypercube: Hypercube;
+
   private cnv: HTMLCanvasElement;
 
-  private ngzone: NgZone;
-
+  private ngzone: NgZone; // for refreshing canvas dimensions
   constructor(ngzone: NgZone) {
     this.ngzone = ngzone;
+    this.cnv = <HTMLCanvasElement>document.getElementById('cmain');
   }
+
   refreshCanvasDimensions() {
-    var cmain: HTMLCanvasElement = <HTMLCanvasElement>document.getElementById('cmain');
-    cmain.width  = cmain.clientWidth;
-    cmain.height = cmain.clientHeight;
+    this.cnv = <HTMLCanvasElement>document.getElementById('cmain');
+    this.cnv.width  = this.cnv.clientWidth;
+    this.cnv.height = this.cnv.clientHeight;
   }
 
   ngOnInit() {
     this.refreshCanvasDimensions();
+    this.createHypercube();
     window.onresize = (e) => {
         this.ngzone.run(() => {
           this.refreshCanvasDimensions();
         });
     };
+    this.animate(this);
   }
 
   setDimensions(n) {
     this.mp.n_dimensions = n;
+    this.createHypercube();
   }
   setDivisions(n_divisions) {
     this.mp.n_divisions = Math.pow(2,n_divisions);
@@ -113,21 +152,59 @@ export class KybosComponent implements OnInit {
 	}
 
   randomizeAngles() {
+    var n_divisions = this.mp.n_divisions;
     for (var i=0; i<this.transforms.length; ++i) {
-      this.transforms[i].angle = Math.random()*Math.PI*2.0;
+      var v = Math.random();
+      var ind = Math.floor(v*n_divisions);
+      this.transforms[i].angle_index = ind;
+      this.transforms[i].angle = v*Math.PI*2;
       this.transforms[i].update();
     }
     this.draw();
   }
 
-  createHypercube() {
+  resetAngles() {
+    for (var i=0; i<this.transforms.length; ++i) {
+      this.transforms[i].angle = 0;
+      this.transforms[i].angle_index = 0;
+      this.transforms[i].update();
+    }
+    this.draw();
+  }
 
+  leftAnimationArrowClicked(i) {
+    this.transforms[i].animate = -1;
+  }
+  rightAnimationArrowClicked(i) {
+    this.transforms[i].animate = 1;
+  }
+  angleChanged(i, v) {
+    this.transforms[i].angle_index = v;
+    this.transforms[i].goal_angle = Math.PI*2*v/this.mp.n_divisions;
+  }
+
+
+  createHypercube() {
+    var n_dimensions = this.mp.n_dimensions;
+    var hc = new Hypercube(n_dimensions);
+    var trn = [];
+    for (var i=0, k=0; i<n_dimensions; ++i) {
+      for (var j=i+1; j<n_dimensions; ++j, ++k) {
+        trn.push(new Transform(0.0, i, j));
+      }
+    }
+    this.hypercube = hc;
+    this.transforms = trn;
+  }
+
+  animate(kbc: KybosComponent) {
+    kbc.draw();
+    window.requestAnimationFrame(function() {
+      kbc.animate(kbc);
+    });
   }
 
   draw() {
-
-    let ctx = this.cnv.getContext('2d');
-    ctx.clearRect(0, 0, this.cnv.width, this.cnv.height);
 
     let speed:number = this.mp.speed;
     let n_divisions:number = this.mp.n_divisions;
@@ -146,6 +223,7 @@ export class KybosComponent implements OnInit {
         trn.update();
       }
 
+      // animate movement
       if (trn.animate != 0) {
 
         var ps = speed + speed*Math.sin(trn.angle*n_divisions-Math.PI/2)*accentuation;
@@ -155,13 +233,43 @@ export class KybosComponent implements OnInit {
         var ind = trn.angle/Math.PI*2*n_divisions;
 				ind = (ind > n_divisions - 0.5)? 0: Math.round(ind);
         trn.angle_index = ind;
-
-
       }
-
     }
+
     
+    let ctx = this.cnv.getContext('2d');
+    ctx.clearRect(0, 0, this.cnv.width, this.cnv.height);
+
+    var w = this.cnv.width;
+    var h = this.cnv.height;
+    var s = Math.min(w, h)/5;
+    var hc = this.hypercube;
+    var trn = this.transforms;
+    var p2d = [];
+    for (var i=0; i<hc.points.length; ++i) {
+      var p = hc.points[i].slice(0);
+      for (var j=0; j<trn.length; ++j) {
+        trn[j].transform(p);
+      }
+      p[0] *= s;
+      p[1] *= s;
+      p[0] += w/2;
+      p[1] = h/2 - p[1];
+      p2d.push(p);
+    }
+
+    ctx.lineWidth = this.mp.line_width;
+    ctx.lineCap = 'round';
+    ctx.strokeStyle = '#FFFFFF';
+    ctx.beginPath();
+    for (var i=0; i<hc.edges.length; ++i) {
+		  ctx.moveTo(p2d[hc.edges[i][0]][0], p2d[hc.edges[i][0]][1]);
+			ctx.lineTo(p2d[hc.edges[i][1]][0], p2d[hc.edges[i][1]][1]);
+		}
+		ctx.stroke();
   }
+
+
 
 
 
